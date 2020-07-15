e1b9462cb66d5a92a320fe2bde9d358e309a4b08
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-07-15 16:58:37 +0100

    hotfix: getAllReplies & getAllComments to return records

--- src/Methods/commentsMethods/getAllComments.js
+++ src/Methods/commentsMethods/getAllComments.js
@@ -1,20 +1,28 @@
 const rp = require('request-promise');
 require('dotenv').config();
-const getAllComments = async (params) => {
+const getAllComments = async (token, pageQuery) => {
   var options = {
     uri: `${process.env.BaseUrl}/v1/comments`,
     headers: {
       'User-Agent': 'Request-Promise',
-      Authorization: `Bearer ${params.appToken}`,
+      Authorization: `Bearer ${token}`,
     },
-    query: pageQuery,
+    qs: pageQuery,
     method: 'GET',
     json: true, // Automatically parses the JSON string in the response
   };
 
   try {
     const getCommentsResponse = await rp(options);
-    return getCommentsResponse;
+    let records = getCommentsResponse.data.records.map((comment) => {
+      return comment;
+    });
+    let pageInfo = getCommentsResponse.data.pageInfo;
+    const data = {
+      records,
+      pageInfo,
+    };
+    return data;
   } catch (error) {
     return error.message;
   }
--- src/Methods/repliesMethods/getAllReplies.js
+++ src/Methods/repliesMethods/getAllReplies.js
@@ -1,21 +1,30 @@
 const rp = require('request-promise');
 require('dotenv').config();
 
-const getAllReplies = async (token, pageQuery) => {
+const getAllReplies = async (token, commentId, pageQuery) => {
+  console.log(pageQuery);
   var options = {
-    uri: `${process.env.BaseUrl}/v1/comments`,
+    uri: `${process.env.BaseUrl}/v1/comments/${commentId}/replies`,
     headers: {
       'User-Agent': 'Request-Promise',
       Authorization: `Bearer ${token}`,
     },
-    query: pageQuery,
+    qs: JSON.stringify(pageQuery),
     method: 'GET',
     json: true, // Automatically parses the JSON string in the response
   };
 
   try {
     const getRepliesResponse = await rp(options);
-    return getRepliesResponse;
+    let records = getRepliesResponse.data.records.map((comment) => {
+      return comment;
+    });
+    let pageInfo = getRepliesResponse.data.pageInfo;
+    const data = {
+      records,
+      pageInfo,
+    };
+    return data;
   } catch (error) {
     return error.message;
   }
